2f3e23633f8a16bbd0974a699ac7e1fd1b456722
Josh Mobley <josh@Joshs-MacBook-Pro.local>
2015-08-04 01:15:47 -0400

    adding logic to subnav

--- nav-page.php
+++ nav-page.php
@@ -4,11 +4,12 @@
 
 		
 
-
+		<?php if(is_archive()) : ?>
 		<h3><?php single_cat_title(); ?></h3>
 		<?php
 		$cat = get_cat_id(single_cat_title('', false));
 		wp_list_categories( array('child_of' => $cat, 'title_li' => '' ));
+	<?php else : ?>
 
 		//GET ID OF TOP-LEVEL PAGE
 		$parent = array_reverse(get_post_ancestors($post->ID));
@@ -22,6 +23,7 @@
 	
 		<ul><?php wp_list_pages( array( 'child_of' => $topID, 'title_li' => '' )  ); ?></ul>
 
+	<?php endif; ?>
 	</div>
 
 	<!--<ul>
