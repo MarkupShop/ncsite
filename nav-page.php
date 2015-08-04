6a1fbf0a740938780aaad4c34ad5aa163535e6f7
Josh Mobley <josh@Joshs-MacBook-Pro.local>
2015-08-04 01:18:01 -0400

    adding logic to subnav

--- nav-page.php
+++ nav-page.php
@@ -4,7 +4,7 @@
 
 		
 
-		<?php if(is_archive()) : ?>
+		<?php if(is_archive() || is_front_page()) : ?>
 		<h3><?php single_cat_title(); ?></h3>
 		<?php
 		$cat = get_cat_id(single_cat_title('', false));
