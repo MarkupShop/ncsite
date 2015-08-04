77be2358d0385e2e68407d60c2803e5ea1256003
Josh Mobley <josh@Joshs-MacBook-Pro.local>
2015-08-03 23:05:16 -0400

    tweaking archive

--- archive.php
+++ archive.php
@@ -23,7 +23,7 @@ include('breadcrumbs.php');
 
 	<article>
 
-		<div class="article-meta">
+		<div class="article-info">
 			<h3 class="article-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
 			<?php the_category(); ?>
 		</div>
