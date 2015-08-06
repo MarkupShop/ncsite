fdbced9fed4e2c3ac5eb702cf939ea257f5e622a
Josh Mobley <josh@Joshs-MacBook-Pro.local>
2015-08-06 09:33:19 -0400

    Adding footer content.

--- footer.php
+++ footer.php
@@ -3,8 +3,76 @@
 <footer>
 
 	<div class="wrapper">
+
+		<div class="column">
 	
-		<h2 class="content-title">Footer TBD</h2>
+			<h4>Contact Us</h4>
+			<p class="address">NCSITE Headquarters<br/>
+				514 Daniels Street, Box 169<br />
+				Raleigh, NC 27605</p>
+	
+			<h4>Email Us</h4>
+			<ul>
+				<li><a href="#">General Inquiries</a></li>
+				<li><a href="#">Membership Inquiries</a></li>
+				<li><a href="#">Operations Inquiries</a></li>
+			</ul>
+
+		</div>
+
+		<div class="column">
+
+			<h4>Connect with NCSITE</h4>
+			<ul>
+				<li><a href="#">NCSITE Twitter</a></li>
+				<li><a href="#">NCSITE Newsstand</a></li>
+				<li><a href="#">Be a Mentor</a></li>
+				<li><a href="#">Advertise with us</a></li>
+			</ul>
+
+			<h4><a href="#">Become a Member</a></h4>
+			<h4><a href="#">Host a Meeting</a></h4>
+			<h4><a href="#">Jobs Board</a></li>
+
+		</div>
+
+		<div class="column">
+
+			<h4>National ITE</h4>
+			<ul>
+				<li><a href="#">ITE Website</a></li>
+				<li><a href="#">ITE Twitter</a></li>
+				<li><a href="#">ITE Facebook</a></li>
+			</ul>
+
+			<h4>ITE Contact</h4>
+			<p class="address">1627 I Street, NW, Suite 600<br/>
+				Washington, DC 20006</p>
+			<p class="phone">202-785-0060</p>
+			<p class="email"><a href="mailto:ite_staff@ite.org">ite_staff@ite.org</a></p>
+
+		</div>
+
+		<div class="column">
+
+			<h4>Transportation Resources</h4>
+			<ul>
+				<li><a href="#">NCDOT</a></li>
+				<li><a href="#">USDOT</a></li>
+				<li><a href="#">Southern District ITE</a></li>
+				<li><a href="#">Bureau Transport. Statistics</a></li>
+				<li><a href="#">Transport. Research Board</a></li>
+				<h4><a href="#">See all Resources</a></h4>
+
+		</div>
+
+		<div class="column">
+
+			<!--widget area-->
+
+		</div>
+
+
 
 	</div>
 
